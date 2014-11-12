bae321f50ec6c446826dfa7ee80cbefc99c76a16
Cosmin Ordean <cosmin.ordean@ropardo.ro>
2014-11-12 14:20:33 +0200

    change server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('test');
+var sys  = require('sys'),
+ my_http = require('http');
+
+my_http.createServer(function(req, res) {
+  sys.puts("Server start");
+  res.writeHeader(200, {"Content-Type": "text/plain"});
+  res.write("Hello World");
+  res.end();
+}).listen(8080);
+sys.puts("Server running on port 8080");
